adb4c8475db402148de672fe72e5eb08358c812f
SondosMatahen <sondosmatahen@gmail.com>
2020-10-02 20:41:10 +0300

    add reservation

---
+++ src/model/reservation/reservation-schema.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const reservation = mongoose.Schema({
+    user_id: {
+        type: String,
+        require: true
+    },
+    provider_id: {
+        type: String,
+        require: true
+    },
+    post_id: {
+        type: String,
+        require: true
+    },
+    book_date: {
+        type: Date,
+        require: true
+    },
+    is_approved: {
+        type: Number,
+        enum: [0, 1, 2],
+        require: true
+        //(0 = pending, 1 = approved, 2 = declined),
+
+    },
+    created_date: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+
+module.exports = mongoose.model('reservation', reservation);
