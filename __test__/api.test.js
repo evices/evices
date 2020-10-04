78fe7c8103726d8b7c6f07b0078f87a24654421f
Nedal_Erekat <nedalerekat@gmail.com>
2020-10-04 23:14:17 +0300

    starting test posting

--- __test__/api.test.js
+++ __test__/api.test.js
@@ -1,29 +1,32 @@
-const { server } = require('../src/server');
+const {
+  server
+} = require('../src/server');
 
 const supergoose = require('@code-fellows/supergoose');
 const mockRequest = supergoose(server);
+const base64 = require('base-64');
 
-describe('SERVER TESTING', ()=> {
-    let trueTestObj = {
-        username: 'hussain1',
-        password: '12345',
-      };
-  it('respond with 201 if: get the route' ,() =>{
-    return mockRequest.get('/user').then(data =>{
+describe('SERVER TESTING', () => {
+  let trueTestObj = {
+    username: 'hussain1',
+    password: '12345',
+  };
+  it('respond with 201 if: get the route', () => {
+    return mockRequest.get('/user').then(data => {
       expect(data.status).toBe(200);
     })
   });
-  it('respond with 201 if: get the route with ID' , async ()  =>{
+  it('respond with 201 if: get the route with ID', async () => {
     let signupResponse = await mockRequest.post('/signup').send(trueTestObj);
-    console.log('signupResponse>>>>>.',signupResponse.body);
+    console.log('signupResponse>>>>>.', signupResponse.body);
 
-    return mockRequest.get(`/user/${signupResponse.body.result._id}`).then(data =>{
-        
+    return mockRequest.get(`/user/${signupResponse.body.result._id}`).then(data => {
+
       expect(data.status).toBe(200);
     })
   });
 
-  it('respond with 201 if: post user using the route' , async () =>{
+  it('respond with 201 if: post user using the route', async () => {
     let obj = {
       username: 'waleed',
       password: 'waleed',
@@ -35,18 +38,51 @@ describe('SERVER TESTING', ()=> {
 
   });
 
-//   it('respond with 201 if: put user into the route' ,() =>{
-//     return mockRequest.put('/user/:id').then(data =>{
-//       expect(data.status).toBe(201);
-//     }).catch(err =>{
-//       console.log(err);
-//     });
-//   });
-//   it('respond with 201 if: delete products using the route' ,() =>{
-//     return mockRequest.delete('api/v1/products/:id').then(data =>{
-//       expect(data.status).toBe(201);
-//     }).catch(err =>{
-//       console.log(err);
-//     });
-//   });
+  //   it('respond with 201 if: put user into the route' ,() =>{
+  //     return mockRequest.put('/user/:id').then(data =>{
+  //       expect(data.status).toBe(201);
+  //     }).catch(err =>{
+  //       console.log(err);
+  //     });
+  //   });
+  //   it('respond with 201 if: delete products using the route' ,() =>{
+  //     return mockRequest.delete('api/v1/products/:id').then(data =>{
+  //       expect(data.status).toBe(201);
+  //     }).catch(err =>{
+  //       console.log(err);
+  //     });
+  //   });
 });
+
+describe('Posting Services on the app', () => {
+  let newUser = {
+    username: "Nedal",
+    password: "1234",
+    role: "seller"
+  }
+  let newUser2 = {
+    username: "hussain",
+    password: "1234",
+    role: "user"
+  }
+  let newPost = {
+    "username": "Nedal",
+    "password": "1234",
+    "title": "create your wall",
+    "description": "call me whenever you want",
+    "category": "TECH"
+  }
+  it('the user can not post without signup as a seller', () => {
+    return mockRequest.post('/post').send(newPost).then(posting=>{
+      expect(posting.status).toBe(500)
+    }).catch(err=>{
+      console.log(err);
+    })
+  })
+
+  it('the user can post if he signed in as a seller',()=>{
+
+    mockRequest.post('/signup').send(newUser)
+    mockRequest.post('/signin').set('authorization', `Basic ${authHeader}`)
+  })
+})
