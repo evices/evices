af8ffaf7ab0099be7a172effd83e266c94b27645
Waleed A. Afifi <waleedafifich@gmail.com>
2020-11-09 11:06:29 +0200

    edit message schemaa

--- src/model/message/message-schema.js
+++ src/model/message/message-schema.js
@@ -6,6 +6,14 @@ const messages = mongoose.Schema({
     receiver_name: String,
     sender_name: String,
     message_text: String,
+    sender_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    receiver_id : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
     is_deleted: Boolean,
     created_date: { type: Date, default: Date.now() }
  });
